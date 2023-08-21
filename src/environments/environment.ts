f8a8f6fa4f263ba13e4cb8bdacb83dce99991901
Abraham Raymundo <70590661+Pimpollo30@users.noreply.github.com>
2023-08-21 13:47:50 -0600

    Heroku

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,3 +1,4 @@
 export const environment = {
-    API_URL: 'http://localhost/portfolio-back/public'
+    // API_URL: 'http://localhost/portfolio-back/public'
+    API_URL: 'https://jarg-backend-146d072e5e9b.herokuapp.com'
 };
